13348ef73bffc19e398ea351edb393215a1c2077
haakonjh <haakonjh@stud.ntnu.no>
2015-01-17 11:22:54 +0100

    Exercise 1

--- Exercise1/threading.py
+++ Exercise1/threading.py
@@ -1,24 +1,21 @@
-from threading import Thread
-
-i = 0
-def thread_1_function():
-	for x in range(0,1000000):
-		i+=1;
-def thread_2_function():
-	for x in range(0,1000000):
-		i-=1;
-
-# Potentially useful thing:
-# In Python you "import" a global variable, instead of "export"ing it when you declare it
-# (This is probably an effort to make you feel bad about typing the word "global")
+from threading import Thread;
+i = 0;
+def thread_function_1():
+	global i;
+	for j in range(0, 1000000):
+		i = i + 1;
+def thread_function_2():
+	global i;
+	for j in range(0, 1000000):
+		i = i - 1;
 def main():
-	thread_1 = Thread(target = thread_1_function, args = (),)
-	thread_2 = Thread(target = thread_2_function, args = (),)
+	global i;
+	thread_1 = Thread(target = thread_function_1, args = ());
+	thread_2 = Thread(target = thread_function_2, args = ());
+	thread_1.start();
+	thread_2.start();
+	thread_1.join();
+	thread_2.join();
+	print(i)
+main();
 
-	thread_1.start()
-	thread_2.start()
-
-	thread_1.join()
-	thread_2.join()
-
-main()
