2f2f05ccd0d1edc1c1b2327a33ff445c007dcf01
haakonjh <haakonjh@stud.ntnu.no>
2015-01-19 13:46:21 +0100

    -

---
+++ Exercise2/threading.c
@@ -0,0 +1,42 @@
+#include <pthread.h>
+#include <stdio.h>
+// Note the return type: void*
+//gcc -std=gnu99 -Wall -g -o helloworld_c helloworld_c.c -lpthread
+static int i=0;
+pthread_mutex_t lock;
+void* thread_1_function(){
+	
+	for(int j=0;j<=1000000;j++)
+	{
+	pthread_mutex_lock(&lock);
+		i++;
+	pthread_mutex_unlock(&lock);
+	}	
+	
+	return NULL;
+}
+
+void* thread_2_function(){
+	
+	for(int j=0;j<=1000000;j++)
+	{
+	pthread_mutex_lock(&lock);
+		i--;
+	pthread_mutex_unlock(&lock);
+	}
+		
+	return NULL;
+}
+
+int main(){
+	pthread_t thread_1;
+	pthread_create(&thread_1, NULL, thread_1_function, &lock);
+	pthread_t thread_2;
+	pthread_create(&thread_2, NULL, thread_2_function, &lock);
+	
+	pthread_join(thread_1, NULL);
+	pthread_join(thread_2, NULL);
+	printf("%i",i);
+	pthread_mutex_destroy(&lock);
+	return 0;
+}
---
+++ Exercise2/threading_py.py
@@ -0,0 +1,25 @@
+from threading import Thread,Lock
+i = 0
+def thread_function_1(lock):
+	global i
+	for j in range(0, 1000000):
+		lock.acquire()
+		i = i + 1
+		lock.release()
+def thread_function_2(lock):
+	global i;
+	for j in range(0, 1000000):
+		lock.acquire()
+		i = i - 1
+		lock.release()
+def main():
+	lock=Lock()
+	thread_1 = Thread(target = thread_function_1, args = (lock))
+	thread_2 = Thread(target = thread_function_2, args = (lock))
+	thread_1.start()
+	thread_2.start()
+	thread_1.join()
+	thread_2.join()
+	print(i)
+main()
+
